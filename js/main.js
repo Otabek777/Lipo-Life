dae9cd3700c322e76f350fbda3560fa4c2413ecd
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-07-13 02:10:36 +0500

    body hidden

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('.burgir').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__navbar').classList.toggle('active');
+    document.querySelector('body').classList.toggle('hidden')
 });
