450bdf2964cc7d5fd8098b057b9d9fc012826382
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-07-15 11:28:11 +0500

    menu

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ document.querySelector('.burgir').addEventListener('click', function() {
     document.querySelector('.header__navbar').classList.toggle('active');
     document.querySelector('body').classList.toggle('hidden')
 });
+
+const headerLink = document.querySelectorAll('.header__navbar li a');
+for(let i = 0; i < headerLink.length; i++) {
+    headerLink[i].addEventListener('click', function() {
+        document.querySelector('.burgir').classList.remove('active');
+        document.querySelector('.header__navbar').classList.remove('active');
+        document.querySelector('body').classList.remove('hidden')
+    });
+};
+
+$(function(){
+    $('.header__navbar li a').click(function(){
+         var target = $(this).attr('href');
+         $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+         return false; 
+         
+    }); 
+ });
